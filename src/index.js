9eee4b93284d3ed37e9303a533fcc70886c112df
unknown <u.web.dev@gmail.com>
2023-08-07 23:53:31 +0300

    mongo to env. update gitignore

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import router from './routes/index.js';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
-mongoose.connect('mongodb+srv://uwebdev:uwebdev2020@recipes.kshlhno.mongodb.net/recipes?retryWrites=true&w=majority');
+mongoose.connect(process.env.DB);
 
 app.use(express.json());
 app.use(cors());
